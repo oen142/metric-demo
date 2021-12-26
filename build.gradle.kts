fdb619a3cc2868a8dd2af03b4835f85292e5ebb8
oen142 <oen142@naver.com>
2021-12-26 21:58:52 +0900

    feat : 패캠강의

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.micrometer:micrometer-registry-prometheus:latest.release")
+    testImplementation("io.micrometer:micrometer-registry-elastic:latest.release")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
